aa8eb5dec3b3f13fdf642839bcc9ea6b00ae4fe1
lichnh <lichnguyenhong.it@gmail.com>
2023-07-04 17:14:57 +0700

    feat: camera follow car

--- Assets/Scripts/FollowPlayer.cs
+++ Assets/Scripts/FollowPlayer.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
-{   
-    public float speed = 5.0f;
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(0, 8, -7);
     // Start is called before the first frame update
     void Start()
     {
@@ -12,9 +13,9 @@ public class PlayerController : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.position =
+            player.transform.position + offset;
     }
 }
