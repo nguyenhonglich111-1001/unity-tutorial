384f552f1aa0f4acfb545282a84fdf7b324f4036
lichnh <lichnguyenhong.it@gmail.com>
2023-07-06 17:39:43 +0700

    hot push

--- Prototype1/Assets/Scripts/PlayerController.cs
+++ Prototype1/Assets/Scripts/PlayerController.cs
@@ -4,7 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {   
-    public float speed = 5.0f;
+    private float speed = 5.0f;
+    private float turnSpeed = 30;
+    private float horizontalInput;
+    private float forwardInput;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        forwardInput = Input.GetAxis("Vertical"); // This is a value between -1 and 1
+        horizontalInput = Input.GetAxis("Horizontal"); // This is a value between -1 and 1
         // Move the vehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        transform.Rotate(Vector3.up, horizontalInput * turnSpeed * Time.deltaTime);
     }
 }
